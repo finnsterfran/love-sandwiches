e0466eb2c31156ca4f8299a7be20bec28f286eb0
finnsterfran <j.frannie.yip@gmail.com>
2021-06-14 09:00:24 +0000

    Adjust validation to check if all values provided are integers

--- run.py
+++ run.py
@@ -12,7 +12,6 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-<<<<<<< HEAD
 def get_sales_data():
     """
     Get sales figure input from the user
@@ -31,7 +30,9 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
+    print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
@@ -39,15 +40,4 @@ def validate_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
 
-
-
 get_sales_data()
-=======
-sales = SHEET.worksheet('sales')
-
-data = sales.get_all_values()
-
-print(data)
-
-  
->>>>>>> 6a61be0b103af0b2865e6af42d409872b04dc304
