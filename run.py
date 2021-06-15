56dd2bebc6b700602259cef2e55b9d1ba6041ee5
finnsterfran <j.frannie.yip@gmail.com>
2021-06-15 06:50:46 +0000

    add user request looop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -16,13 +16,22 @@ def get_sales_data():
     """
     Get sales figure input from the user
     """
-    print("Please enter sales deata from last market")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10, 20, 30, 40, 50, 60\n")
-    data_str = input("Enter your data here: ")
+    while True: 
+
+        print("Please enter sales deata from last market")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10, 20, 30, 40, 50, 60\n")
+
+        data_str = input("Enter your data here: ")
+        
+        sales_data = data_str.split(",")
+        
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
+
+    return sales_data
     
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
 
 def validate_data(values):
     """
@@ -39,5 +48,8 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-get_sales_data()
+    return True
+
+data = get_sales_data()
